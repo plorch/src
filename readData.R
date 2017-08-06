be23b6490436d1d6a3f1d6a75c39f7b9032de02c
Pat Lorch <pat_lorch@yahoo.com>
2017-08-06 10:25:30 -0700

    -m 'added a space'

--- readData.R
+++ readData.R
@@ -1,5 +1,5 @@
 #' This script will read in raw data from the Bahamas Marine Mammal Research Organization
 #' into a data frame called whales. whales will serve as the intermediate data for
-#' subsequent analysis
+#'  subsequent analysis
 whales <- read.csv(file = '../data/bbmroData.csv')
 head(whales)
